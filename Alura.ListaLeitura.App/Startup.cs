1e26eae05714b89be6ea5639d9edd64e8b00de19
rodrigooszter@outlook.com <rodrigooszter@outlook.com>
2021-04-27 21:02:29 -0300

    Initial commit

---
+++ Alura.ListaLeitura.App/Startup.cs
@@ -0,0 +1,22 @@
+﻿using Alura.ListaLeitura.App.Repositorio;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace Alura.ListaLeitura.App
+{
+    internal class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Run(ParaLer);
+
+        }
+
+        public Task ParaLer(HttpContext context)
+        {
+            var _paraLer = new LivroRepositorioCSV();
+            return context.Response.WriteAsync(_paraLer.Lidos.ToString());
+        }
+    }
+}
---
+++ Alura.ListaLeitura.App/bin/Debug/netcoreapp2.0/Program.cs
@@ -0,0 +1,35 @@
+﻿using Alura.ListaLeitura.App.Negocio;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+using System;
+using System.IO;
+
+namespace Alura.ListaLeitura.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("hosting.json", optional: false)
+                .Build();
+
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseConfiguration(config)
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+            host.Run();
+        }
+
+        static void ImprimeLista(ListaDeLeitura lista)
+        {
+            Console.WriteLine(lista);
+        }
+    }
+}
